97458652ccf8bdbd5f4a391b024f6ba86236f562
Rismaputri <rismaputrisma@gmail.com>
2019-10-29 14:46:49 +0700

    cari_motor_sesuai

--- dbasee/customer.py
+++ dbasee/customer.py
@@ -28,3 +28,15 @@ class Customer:
         else:
             hasil = '7 naga'
         print(f'Check gaji {self.nama} sebesar {self.gaji}, status {hasil}')
+
+    def cari_motor(self):
+        if self.gaji <= 3000000:
+            hasil = '< 3Jt, Motor Beat'
+        elif self.gaji > 3000000 and self.gaji <= 6000000:
+            hasil = 'Menengah, Motor Vario'
+        elif self.gaji > 6000000 and self.gaji <= 9000000:
+            hasil = 'Atas, Motor N-Max'
+        else:
+            hasil = '7 naga, Motor Premium'
+        print(
+            f'Cari motor dengan {self.nama} dan gaji sebesar {self.gaji}, maka status dan motornya adalah {hasil}')
